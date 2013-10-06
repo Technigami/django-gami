b5bf82ceab007ddf022929efaa6a0c89eda75075
Daniel Gray <dan@technigami.com>
2013-10-06 12:53:29 -0700

    updating to work as bash script.

--- gami/bin/gami.py
+++ gami/bin/gami.py
@@ -1,6 +1,41 @@
+#!/usr/bin/env python
+
 from fabric.tasks import execute
-from fabfile import clone,ls
+from fabric.colors import yellow, green, blue, red
+from fabric.api import env, cd, prefix, sudo as _sudo, run as _run, hide, task
+
+from fabfile import *
+import sys
+
+
+###########################################
+# Utils and wrappers for various commands #
+###########################################
+
+# from Mezzanine::
+
+def _print(output):
+    print
+    print output
+    print
+
+
+def print_command(command):
+    _print(blue("$ ", bold=True) +
+           yellow(command, bold=True) +
+           red(" ->", bold=True))
+           
+           
+def command_switcher(arguments):
+    if len(arguments) < 2:
+        _print(red("Command not found.  Here is a list of the available commands.", bold=True))
+        _print(green(local('fab --list')))
+    else:
+        cmd_string = "'" + "','".join(arguments[2:])+"'" 
+        local("fab "+arguments[1] + ":" + cmd_string)
+        
+
 
 if __name__ == "__main__":
-    execute(clone)
-
+    # Parse command line arguments for gami
+    command_switcher(sys.argv)
